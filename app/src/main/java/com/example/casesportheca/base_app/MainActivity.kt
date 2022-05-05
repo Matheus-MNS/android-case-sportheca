dbd9d78aa176a9e6eef12302eaa6676622bc10b3
Matheus Mendes <contact.matheus.mendes@gmail.com>
2022-05-05 18:41:51 -0300

    aap structured and navigation implemented

---
+++ app/src/main/java/com/example/casesportheca/base_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.casesportheca.base_app
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.casesportheca.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main)
---
+++ app/src/main/java/com/example/casesportheca/feature/world_cup/domain/Placeholder.kt
@@ -0,0 +1,2 @@
+package com.example.casesportheca.feature.world_cup.domain
+
